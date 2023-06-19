c12f3ac25769bbc4945008d5ea8629f1ffd58ddb
Sheila Seruya <Sheilaseruya@gmail.com>
2023-06-19 14:20:57 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: 'Leo', last_name: 'Mateo', grade: 50 )
+Student.create(first_name: 'Tina', last_name: 'Namjoon', grade: 60 )
+Student.create(first_name: 'Martha', last_name: 'Yoongi', grade: 80 )
+Student.create(first_name: 'Selena', last_name: 'Seok', grade: 90 )
+Student.create(first_name: 'Nabi', last_name: 'Park', grade: 80 )
